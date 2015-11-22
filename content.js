3c92940aff9ea8d2d4969666fdde1fd3d0f94adb
Emily Wang <emilyluwang@college.harvard.edu>
2015-11-22 16:36:11 -0500

    adding test code

--- content.js
+++ content.js
@@ -1,21 +1,9 @@
-// content.js
-var href0 = $("a[href^='http']").eq(0).attr("href");
-var href1 = $("a[href^='http']").eq(1).attr("href");
-var allHref = $("a[href^='http']").attr("href");
-
-console.log('0');
-console.log(href0);
-console.log('1');
-console.log(href1);
-console.log('all');
-console.log(allHref);
-
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if(request.message === "clicked_browser_action") {
-		for (var i = 0; i < $("a[href^='http']").length; i++) {
-			console.log($("a[href^='http']").eq(i).attr("href"));
-		}
-    }
-  }
+	function(request, sender, sendResponse) {
+    	if(request.message === "clicked_browser_action") {
+			for (var i = 0; i < $("a[href^='http']").length; i++) {
+				console.log($("a[href^='http']").eq(i).attr("href"));
+			}
+    	}
+  	}
 );
