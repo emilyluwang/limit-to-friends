025ad1e81df8233b11322d9652af19fff10e245f
Emily Wang <emilyluwang@college.harvard.edu>
2015-11-23 12:52:28 -0500

    registered API on fb, should be able to make calls now

--- background.js
+++ background.js
@@ -1,10 +1,32 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
+/**chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
-});
+});**/
+
+var successURL = 'https://www.facebook.com/connect/login_success.html';
+//var successURL = 'https://www.facebook.com/connect/blank.html#_=_'
+
+function onFacebookLogin() {
+	if (!localStorage.accessToken) {
+		alert("Hello");
+		chrome.tabs.getAllInWindow(null, function(tabs) {
+			for (var i = 0; i < tabs.length; i++) {
+				if (tabs[i].url.indexOf(successURL) == 0) {
+					var params = tabs[i].url.split('#')[1];
+					access = params.split('&')[0]
+					console.log(access);
+					localStorage.accessToken = access;
+					chrome.tabs.onUpdated.removeListener(onFacebookLogin);
+					return;
+				}
+			}
+		});
+ 	}
+}
+chrome.tabs.onUpdated.addListener(onFacebookLogin);
--- popup.js
+++ popup.js
@@ -1,4 +1,17 @@
-console.log("Inside content.js");
+if (localStorage.accessToken) {
+	var graphUrl = "https://graph.facebook.com/me?" + localStorage.accessToken + "&callback=displayUser";
+	console.log(graphUrl);
+ 
+	var script = document.createElement("script");
+	script.src = graphUrl;
+	document.body.appendChild(script);
+ 
+	function displayUser(user) {
+		console.log(user);
+	}
+}
+/**
+console.log("Inside popup.js");
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
@@ -17,4 +30,4 @@ chrome.runtime.onMessage.addListener(
 			}
     	}
   	}
-);
+);**/
