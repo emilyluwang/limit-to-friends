a9d0970ade3bc55f784c8b81c0fd98a53c8920fa
Emily Wang <emilyluwang@college.harvard.edu>
2015-11-25 16:58:29 -0800

    getting user ids

--- background.js
+++ background.js
@@ -1,17 +1,5 @@
 // background.js
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
-});
-
 var successURL = 'https://www.facebook.com/connect/login_success.html';
-var redirectURL = 'https://www.facebook.com/connect/blank.html#_=_'
-//alert(successURL);
 
 function onFacebookLogin() {
 	chrome.tabs.getAllInWindow(null, function(tabs) {
@@ -20,7 +8,7 @@ function onFacebookLogin() {
 				//alert(tabs[i].url);
 				var params = tabs[i].url.split('#')[1];
 				access = params.split('&')[0]
-				alert(access);
+				//alert(access);
 				localStorage.accessToken = access;
 				chrome.tabs.remove(i, function() { });
 				//alert(localStorage.accessToken);
@@ -32,4 +20,5 @@ function onFacebookLogin() {
 }
 
 chrome.tabs.onUpdated.addListener(onFacebookLogin);
+
 
--- content.js
+++ content.js
@@ -1,32 +1,23 @@
-//console.log("Inside content.js");
 
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-    	if (request.message === "clicked_browser_action") {
-    		console.log("Inside the if block");
-    		alert("Setting to null");
-    		localStorage.accessToken = null;
-    		alert(localStorage.accessToken);
-
-			//for (var i = 0; i < $("a[href^='http']").length; i++) {
-			//	console.log($("a[href^='http']").eq(i).attr("href"));
-			//}
-    	}
-  	}
-);
 //alert("In content.js");
 if (localStorage.accessToken) {
+	alert(document.getElementById("status").value);
+
 	//alert("in content.js");
-	var graphUrl = "https://graph.facebook.com/me/friendlists?" + localStorage.accessToken + "&callback=displayUser";
+	var graphUrl = "https://graph.facebook.com/me/taggable_friends?" + localStorage.accessToken + "&callback=displayUser";
 	alert(graphUrl);
- 
+	var friendIds = new Array();
+
 	var script = document.createElement("script");
 	script.src = graphUrl;
 	document.body.appendChild(script);
- 
-	function displayUser(user) {
-		alert("Hello from the outside");
-		console.log(user.name);
-		console.log(user.id);
+
+	function displayUser(info) {
+		console.log(info);
+		for (var i = 0; i < info.data.length; i++) {
+			friendIds.push(info.data[i].id);
+		}
+		console.log(friendIds);
+		console.log(document.getElementById("status").value);
 	}
 }
