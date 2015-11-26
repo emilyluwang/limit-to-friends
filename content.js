0bac7079582ece638ef5085151d6bb37806e5338
Emily Wang <emilyluwang@college.harvard.edu>
2015-11-25 17:52:44 -0800

    adding ability to post without custom friend list

--- content.js
+++ content.js
@@ -1,12 +1,17 @@
+document.getElementById("status_button").addEventListener("click", saveStatus);
 
+function saveStatus() {
+	localStorage.newStatus = document.getElementById("status").value;
+}
 //alert("In content.js");
-if (localStorage.accessToken) {
-	alert(document.getElementById("status").value);
+if (localStorage.accessToken && localStorage.newStatus) {
+	alert(localStorage.newStatus);
 
 	//alert("in content.js");
 	var graphUrl = "https://graph.facebook.com/me/taggable_friends?" + localStorage.accessToken + "&callback=displayUser";
 	alert(graphUrl);
 	var friendIds = new Array();
+	localStorage.friendString = ""
 
 	var script = document.createElement("script");
 	script.src = graphUrl;
@@ -15,9 +20,28 @@ if (localStorage.accessToken) {
 	function displayUser(info) {
 		console.log(info);
 		for (var i = 0; i < info.data.length; i++) {
-			friendIds.push(info.data[i].id);
+			localStorage.friendString = localStorage.friendString.concat(",");
+			localStorage.friendString = localStorage.friendString.concat(info.data[i].id);
 		}
-		console.log(friendIds);
-		console.log(document.getElementById("status").value);
-	}
+		localStorage.friendString = localStorage.friendString.substring(1);
+		localStorage.friendString = localStorage.friendString.substring(0, localStorage.friendString.length - 1);
+		
+		console.log(localStorage.friendString);
+
+		var form = document.createElement("form");
+    	form.setAttribute("method", "post");
+    	form.setAttribute("action", "https://graph.facebook.com/me/feed?" + localStorage.accessToken);
+
+    	var hiddenField1 = document.createElement("input");
+    	hiddenField1.setAttribute("name", "message");
+    	hiddenField1.setAttribute("value", localStorage.newStatus);
+
+    	form.appendChild(hiddenField1);
+
+    	document.body.appendChild(form);
+    	form.submit();
+
+    	localStorage.newStatus = null;
+		
+		}
 }
