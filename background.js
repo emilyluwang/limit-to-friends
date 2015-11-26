dd80b07c2ce5093954c5db4afbf7bd084630cae5
Emily Wang <emilyluwang@college.harvard.edu>
2015-11-25 21:00:23 -0800

    commenting/documentation

--- background.js
+++ background.js
@@ -1,18 +1,15 @@
 // background.js
 var successURL = 'https://www.facebook.com/connect/login_success.html';
 
+// Get the access token
 function onFacebookLogin() {
 	chrome.tabs.getAllInWindow(null, function(tabs) {
 		for (var i = 0; i < tabs.length; i++) {
 			if (tabs[i].url.indexOf(successURL) == 0) {
-				//alert(tabs[i].url);
 				var params = tabs[i].url.split('#')[1];
 				access = params.split('&')[0]
-				//alert(access);
 				localStorage.accessToken = access;
 				chrome.tabs.remove(i, function() { });
-				//alert(localStorage.accessToken);
-				//chrome.tabs.onUpdated.removeListener(onFacebookLogin);
 				return;
 			}
 		}
--- content.js
+++ content.js
@@ -1,24 +1,34 @@
+// To give credit where credit is due: thank you to github user nobuf
+// This extension builds off the structure of https://github.com/nobuf/facebook-connect-for-chrome-extension
+// content.js
+// Listen for the status being entered
 document.getElementById("status_button").addEventListener("click", saveStatus);
 
+// Save the entered status into local storage
 function saveStatus() {
 	localStorage.newStatus = document.getElementById("status").value;
+	localStorage.shouldUpdate = true;
 }
-//alert("In content.js");
-if (localStorage.accessToken && localStorage.newStatus) {
-	alert(localStorage.newStatus);
 
-	//alert("in content.js");
-	var graphUrl = "https://graph.facebook.com/me/taggable_friends?" + localStorage.accessToken + "&callback=displayUser";
-	alert(graphUrl);
+// If the status is new and the access token is valid, request info from the FB graph api
+if (localStorage.accessToken && localStorage.shouldUpdate) {
+
+	// Prepare FB graph api request.
+	// NOTE: Because of the updated graph api settings, the only friend ids accessible will be limited
+	// Only friends using the app will have their ids appear in this list.
+	var graphUrl = "https://graph.facebook.com/me/friends?" + localStorage.accessToken + "&callback=displayUser";
 	var friendIds = new Array();
 	localStorage.friendString = ""
 
+	// Execute FB graph api request to get friend ids.
 	var script = document.createElement("script");
 	script.src = graphUrl;
 	document.body.appendChild(script);
 
+	// Callback function once data has been returned.
 	function displayUser(info) {
-		console.log(info);
+
+		// Format friend ids into comma separated string
 		for (var i = 0; i < info.data.length; i++) {
 			localStorage.friendString = localStorage.friendString.concat(",");
 			localStorage.friendString = localStorage.friendString.concat(info.data[i].id);
@@ -26,22 +36,32 @@ if (localStorage.accessToken && localStorage.newStatus) {
 		localStorage.friendString = localStorage.friendString.substring(1);
 		localStorage.friendString = localStorage.friendString.substring(0, localStorage.friendString.length - 1);
 		
-		console.log(localStorage.friendString);
-
+		// Prepare FB graph api POST request to post FB status with sharing permissions.
+		// If the friend id list is empty, the status will be private.
+		// Else, the status will be visible to only friends using the extension. 
 		var form = document.createElement("form");
     	form.setAttribute("method", "post");
     	form.setAttribute("action", "https://graph.facebook.com/me/feed?" + localStorage.accessToken);
 
-    	var hiddenField1 = document.createElement("input");
-    	hiddenField1.setAttribute("name", "message");
-    	hiddenField1.setAttribute("value", localStorage.newStatus);
+    	var statusField = document.createElement("input");
+    	statusField.setAttribute("name", "message");
+    	statusField.setAttribute("value", localStorage.newStatus);
+    	form.appendChild(statusField);
 
-    	form.appendChild(hiddenField1);
+    	var sharingField = document.createElement("input");
+    	sharingField.setAttribute("name", "privacy");
+    	if (localStorage.friendString.length == 0) {
+    		privacyObject = '{"value": "SELF"}';
+    	} else {
+    		var privacyObject = '{"value": "CUSTOM", "allow":"';
+    		privacyObject = privacyObject.concat(localStorage.friendString);
+    		privacyObject = privacyObject.concat('"}');
+    	}
 
-    	document.body.appendChild(form);
+    	sharingField.setAttribute("value", privacyObject);
+    	form.appendChild(sharingField);
+
+    	localStorage.shouldUpdate = false;
     	form.submit();
-
-    	localStorage.newStatus = null;
-		
 		}
 }
